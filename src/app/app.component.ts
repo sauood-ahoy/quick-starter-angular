d655cf8aa2e710246a089455c4acc33a96dcc71a
sauood-ahoy <m.meeran@theahoyapp.com>
2022-08-09 21:07:37 +0400

    init map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component, VERSION } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SUBSCRIPTION_KEY } from '../constants';
+declare var AhoyMapView: any;
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements AfterViewInit {
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
+  map: any;
+  ui: any;
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initializeMap();
+  }
+
+  private async initializeMap() {
+    let platform = await AhoyMapView.service().Platform(SUBSCRIPTION_KEY);
+    const defaultLayers = platform.createDefaultLayers();
+
+    this.map = AhoyMapView.Map(
+      this.mapContainer.nativeElement,
+      defaultLayers.vector.normal.map,
+      {
+        zoom: 4,
+        center: {
+          lat: 34,
+          lng: 41,
+        },
+      }
+    );
+
+    // default interactions for pan/zoom
+    const mapEvents = AhoyMapView.mapevents().MapEvents(this.map);
+    const behavior = AhoyMapView.mapevents().Behavior(mapEvents);
+
+    // Create the default UI components
+    this.ui = AhoyMapView.ui().UI.createDefault(this.map, defaultLayers);
+  }
 }
